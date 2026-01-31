efda96ddccb8f111065ebb5a8975dc52b2df62b7
h773859 <h773859@stud.u-szeged.hu>
2026-02-01 00:31:35 +0100

    j

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
       resolveAlias: {
         // This helps Turbopack find the generated client files 
         // that your Better-Auth and Prisma lib are looking for.
-        '@prisma/client': '@prisma/client',
+        '@prisma/client': '../../node_modules/.prisma/client',
       },
     },
   },
